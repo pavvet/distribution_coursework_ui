d5410efab0ce80d7bbf4d655e13d4b56015de498
Pavel <vetlyugaev@yandex.ru>
2022-10-07 09:36:19 +0300

    Убраны лишние файлы

--- android/app/src/main/kotlin/com/example/distribution_coursework/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.distribution_coursework
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:distribution_coursework/app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(RootApp());
-}
